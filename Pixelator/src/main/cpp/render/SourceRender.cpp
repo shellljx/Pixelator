f4f28fe6f0ca47a0314b8c978dc5377c3031e08a
李金祥 <lijinxiang@shizhuang-inc.com>
2023-05-10 11:30:34 +0800

    feat: add source image render
    #branch=main

---
+++ Pixelator/src/main/cpp/render/SourceRender.cpp
@@ -0,0 +1,5 @@
+//
+// Created by shell on 2023/5/10.
+//
+
+#include "SourceRender.h"
---
+++ Pixelator/src/main/cpp/render/SourceRender.h
@@ -0,0 +1,14 @@
+//
+// Created by shell m1 on 2023/5/10.
+//
+
+#ifndef PIXELATE_SOURCERENDER_H
+#define PIXELATE_SOURCERENDER_H
+
+
+class SourceRender {
+
+};
+
+
+#endif //PIXELATE_SOURCERENDER_H
