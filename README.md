83cc9fcabf8f67d28ff88b062c1462e4bdbdb371
shell <shellljx@gmail.com>
2023-07-28 15:03:36 +0800

    feat: 集成 firebase crashlytics
    #branch=main
    #targetBranch=6513d3f Merge pull request #3 from shellljx/feat/renderer

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 1. 需要确保每次opengl相关操作时都要正确的makecurrent到对应到eglsurface
 2. 删除纹理只是标记了该纹理可以删除，调用了swapbuffers之后，所有绘制操作结束，交换了缓存，才会真正清理
+
+
+1. 先打 release
+2. 执行 ./gradlew app:uploadCrashlyticsSymbolFileRelease 上传符号表
--- app/src/main/java/com/gmail/shellljx/pixelate/MyApplication.kt
+++ app/src/main/java/com/gmail/shellljx/pixelate/MyApplication.kt
@@ -1,13 +1,13 @@
 package com.gmail.shellljx.pixelate
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.gmail.shellljx.pixelate.utils.DensityUtils
 
-class MyApplication : Application() {
+class MyApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         DensityUtils.init(this)
-        Fresco.initialize(this);
+        Fresco.initialize(this)
     }
 }
